2cad93ae9a37581193998a8550885fb9bc7772f9
Cihan Cinar <cihancnr@gmail.com>
2021-02-26 11:03:21 +0000

    Init website

---
+++ precache-manifest.011bdd7090a41e686e49953c71cd936d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538718bd71dffea917277d158b809167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9f3e6e832f5c3caedd",
+    "url": "/static/css/main.623a33a7.chunk.css"
+  },
+  {
+    "revision": "4521a9d863a812a6ca8b",
+    "url": "/static/js/2.f50814c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f50814c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9f3e6e832f5c3caedd",
+    "url": "/static/js/main.3684e92e.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/static/js/main.3684e92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719c09d18e5241a6d524",
+    "url": "/static/js/runtime-main.5690982f.js"
+  },
+  {
+    "revision": "34c74b21ab207db146b57a2aa79b818a",
+    "url": "/static/media/Resume_Cihan-CINAR_Senior-SE.34c74b21.pdf"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "4c5dc2241db2910ee316858638d0dfa3",
+    "url": "/static/media/avatar.4c5dc224.png"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "6f29e095d94d4113829527f189b93a65",
+    "url": "/static/media/ewl.6f29e095.PNG"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "bca2f909f14498e90d19a2c1e01dcec9",
+    "url": "/static/media/liris.bca2f909.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "86d123b294c2209df986cadc5f684450",
+    "url": "/static/media/vcard.86d123b2.png"
+  }
+]);
