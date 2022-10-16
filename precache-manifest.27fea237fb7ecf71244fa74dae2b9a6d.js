1bdf4d3b9061a90f3611040b22b2574aa7d259bc
Cihan Cinar <cihancnr@gmail.com>
2022-10-17 01:42:14 +0200

    Add new Givaudan Experience

---
+++ precache-manifest.27fea237fb7ecf71244fa74dae2b9a6d.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918bcdfea00f9db6de1fc7c0f33ae16b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc4c6d1dcda044cf071",
+    "url": "/static/css/main.5d210586.chunk.css"
+  },
+  {
+    "revision": "55f2828f6949481381ba",
+    "url": "/static/js/2.c9b617e6.chunk.js"
+  },
+  {
+    "revision": "1c444b1a0550833bb8e9c87ea650dbf4",
+    "url": "/static/js/2.c9b617e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc4c6d1dcda044cf071",
+    "url": "/static/js/main.25ec23a1.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/static/js/main.25ec23a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4772a78bdc5eea047a43",
+    "url": "/static/js/runtime-main.6ecd090b.js"
+  },
+  {
+    "revision": "26cab9e79640342d0df88aa35d4b5168",
+    "url": "/static/media/CV_Cihan-CINAR.26cab9e7.pdf"
+  },
+  {
+    "revision": "2fc1f06fb954a0dc0b48c84b95195bea",
+    "url": "/static/media/Technical_Expert_Cihan_CINAR_Worldline.2fc1f06f.pdf"
+  },
+  {
+    "revision": "c4f53128f51ab2014cc94ecb7b420fea",
+    "url": "/static/media/angular.c4f53128.webp"
+  },
+  {
+    "revision": "f0988582a09538bdfd4e9a72ecc709dc",
+    "url": "/static/media/apigee.f0988582.webp"
+  },
+  {
+    "revision": "2bf684e8fe19e03c10f13af615f7c266",
+    "url": "/static/media/apple-icon.2bf684e8.webp"
+  },
+  {
+    "revision": "2e3e2de02c1a76f758648d0508a0258f",
+    "url": "/static/media/atoswl.2e3e2de0.webp"
+  },
+  {
+    "revision": "6b15de18b4e64acffbf11ad5ab51abd0",
+    "url": "/static/media/blurred-image-1.6b15de18.webp"
+  },
+  {
+    "revision": "acca990f97f2559603b359c782e251d7",
+    "url": "/static/media/clouds.acca990f.webp"
+  },
+  {
+    "revision": "e95115b5f3562786257c53d3ea1db56d",
+    "url": "/static/media/docker.e95115b5.webp"
+  },
+  {
+    "revision": "e33aafffb9bc51d68bd461f70873309d",
+    "url": "/static/media/ember.e33aafff.webp"
+  },
+  {
+    "revision": "b03aafb227b4c9832703ede9f45b30c4",
+    "url": "/static/media/ewl.b03aafb2.webp"
+  },
+  {
+    "revision": "129fb6ee5e0a90095dfba15b6f15c324",
+    "url": "/static/media/favicon.129fb6ee.ico"
+  },
+  {
+    "revision": "80f76d3b2ce50fd6e0fb7c8e428638f4",
+    "url": "/static/media/favicon.80f76d3b.webp"
+  },
+  {
+    "revision": "76fdab1b1e98c22711ef32d20ac3fb7a",
+    "url": "/static/media/fog-low.76fdab1b.webp"
+  },
+  {
+    "revision": "edb3e8d028b8efbfa8245da5908f274e",
+    "url": "/static/media/france.edb3e8d0.webp"
+  },
+  {
+    "revision": "61fcdaf1b23d275aecc7ec1ff4ad17d1",
+    "url": "/static/media/gitlab.61fcdaf1.webp"
+  },
+  {
+    "revision": "b6c7385c71ea651c692b12f6b3d95c03",
+    "url": "/static/media/givaudan.b6c7385c.webp"
+  },
+  {
+    "revision": "0f4dc116ee8e70be00b7b370b53b3072",
+    "url": "/static/media/header.0f4dc116.webp"
+  },
+  {
+    "revision": "78f4f0e3e5a7bf6be390a1ddcdb4d46f",
+    "url": "/static/media/helm.78f4f0e3.svg"
+  },
+  {
+    "revision": "ec3463b09e6706257a9a17f2608ef21b",
+    "url": "/static/media/iut.ec3463b0.webp"
+  },
+  {
+    "revision": "84ea44390556d6fb90a277ad2d53f232",
+    "url": "/static/media/kafka.84ea4439.webp"
+  },
+  {
+    "revision": "9066f823125156cce0790e00f5e09aa8",
+    "url": "/static/media/liris.9066f823.webp"
+  },
+  {
+    "revision": "3bd536945de9219c925c55920d472337",
+    "url": "/static/media/microservices.3bd53694.webp"
+  },
+  {
+    "revision": "0082f1ff7c2ec2c0e8155b2b88f6b791",
+    "url": "/static/media/mongodb.0082f1ff.webp"
+  },
+  {
+    "revision": "7d3429d61ad814afb770de020d29e09a",
+    "url": "/static/media/mysql.7d3429d6.webp"
+  },
+  {
+    "revision": "ecf4570d5b4f2893b3305e9d72e62a65",
+    "url": "/static/media/nodejs.ecf4570d.webp"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "92e7581ab14047afd5688bed0132b2c0",
+    "url": "/static/media/nucleo-logo.92e7581a.svg"
+  },
+  {
+    "revision": "09d43565ef9686ea63163725698120ea",
+    "url": "/static/media/openshift.09d43565.svg"
+  },
+  {
+    "revision": "a87e1cdcce09ec17b559ff3a7eb5df58",
+    "url": "/static/media/quarkus.a87e1cdc.webp"
+  },
+  {
+    "revision": "473210eead82401ec0b3b05496045f76",
+    "url": "/static/media/react.473210ee.webp"
+  },
+  {
+    "revision": "eda1c6374d3979144bf128c687cb6e66",
+    "url": "/static/media/redis.eda1c637.svg"
+  },
+  {
+    "revision": "bd2949102089f47ebc6fe9b9a7ae9ab5",
+    "url": "/static/media/rest.bd294910.webp"
+  },
+  {
+    "revision": "6053f5d238dbdda3eb6b9a25369d439d",
+    "url": "/static/media/soap.6053f5d2.webp"
+  },
+  {
+    "revision": "2de5da2c61932a972c895e77b42b5b3c",
+    "url": "/static/media/spring.2de5da2c.webp"
+  },
+  {
+    "revision": "15261516ac00eb543d3c29f766213e4c",
+    "url": "/static/media/top0.15261516.webp"
+  },
+  {
+    "revision": "233c5be734a4adfd3d77183615dc3e78",
+    "url": "/static/media/top1.233c5be7.webp"
+  },
+  {
+    "revision": "96129ab3a85fb00134c2641dec247285",
+    "url": "/static/media/turkey.96129ab3.webp"
+  },
+  {
+    "revision": "f3a63ab8ec3136ca190648f3ddeb3773",
+    "url": "/static/media/ucbl.f3a63ab8.webp"
+  },
+  {
+    "revision": "3cde7107eae5300fdcfc1eeee7931a08",
+    "url": "/static/media/uk.3cde7107.webp"
+  },
+  {
+    "revision": "3a9a5ee3e702c4e4ef6c8adb545f5f4c",
+    "url": "/static/media/vcard.3a9a5ee3.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1257b4ebaa94ba04e395d44ce9202c17.js"
+  "/precache-manifest.27fea237fb7ecf71244fa74dae2b9a6d.js"
 );
 
 self.addEventListener('message', (event) => {
